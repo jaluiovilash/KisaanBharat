8af011d5d344723a8f860b08eddaa54127c028e2
jaluiovilash <ovilashjalui@gmail.com>
2024-06-23 00:37:28 +0530

    updated

--- JS/script.js
+++ JS/script.js
@@ -1,30 +1,9 @@
 // progress bar code
 window.onscroll = function () {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
     document.getElementById("myBar").style.width = scrolled + "%";
 }
 
 
-
-// for scrolling down smooth on clicking browse button
-
-$(document).ready(function () {
-    $('a[href*=#]').bind('click', function (e) {
-        e.preventDefault();
-        var target = this.hash;
-        var $target = $(target);
-
-        if ($target.length) {
-            var weAreHere = $("#myDivWithScrollBar").scrollTop();
-            var distance = $target.offset().top - 220;
-            var weAreGoing = weAreHere + distance;
-
-            $('#myDivWithScrollBar').animate({
-                scrollTop: weAreGoing
-            }, 1000); // duration set to 1000 milliseconds (1 second)
-            return false;
-        }
-    });
-});
